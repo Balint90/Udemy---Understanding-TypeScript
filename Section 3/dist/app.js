83653e2c6522a52f9638fc800d01dc0f397e2f07
Bálint <csertanb@gmail.com>
2026-05-14 17:12:58 +0200

    Done - Section 3

---
+++ Section 3/dist/app.js
@@ -0,0 +1,9 @@
+import fs from "node:fs";
+fs.readFileSync();
+let userName;
+userName = "Balint";
+console.log(userName);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(4, 6));
---
+++ Section 3/src/app.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs";
+
+fs.readFileSync();
+
+let userName: string;
+
+userName = "Balint";
+
+console.log(userName);
+
+function add(a: any, b: any) {
+  return a + b;
+}
+
+console.log(add(4, 6));
